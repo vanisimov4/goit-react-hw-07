f7414222633d742a0761b34ce8d470516b69e193
Volodymyr Anisimov <144102084+vanisimov4@users.noreply.github.com>
2025-05-27 21:07:32 +0300

    createAsyncThunk

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,22 +1,37 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchInProgress, fetchSuccess, fetchError } from "./contactsSlice";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+// import { fetchInProgress, fetchSuccess, fetchError } from './contactsSlice';
 
 // Встановлюємо базову URL-адресу
 // для всіх запитів axios
-axios.defaults.baseURL = "https://683576c8cd78db2058c19d08.mockapi.io";
+axios.defaults.baseURL = 'https://683576c8cd78db2058c19d08.mockapi.io';
 
-export const fetchContacts = () => async (dispatch) => {
-  try {
-    // Індикатор завантаження
-    dispatch(fetchInProgress());
-    // HTTP-запит
-    const response = await axios.get("/contacts");
-
-    // Обробка даних
-    dispatch(fetchSuccess(response.data));
-  } catch (e) {
-    // Обробка помилки
-    dispatch(fetchError(e.message));
-  }
-};
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) =>
+    // async dispatch =>
+    {
+      try {
+        const response = await axios.get('/contacts');
+        // При успішному запиті повертаємо проміс із даними
+        return response.data;
+      } catch (e) {
+        // При помилці запиту повертаємо проміс
+        // який буде відхилений з текстом помилки
+        return thunkAPI.rejectWithValue(e.message);
+      }
+      // const response = await axios.get('/contacts');
+      // return response.data;
+      // try {
+      //   // Індикатор завантаження
+      //   dispatch(fetchInProgress());
+      //   // HTTP-запит
+      //   const response = await axios.get('/contacts');
+      //   // Обробка даних
+      //   dispatch(fetchSuccess(response.data));
+      // } catch (e) {
+      //   // Обробка помилки
+      //   dispatch(fetchError(e.message));
+      // }
+    }
+);
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,43 +1,60 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts } from './contactsOps';
 
 const slice = createSlice({
   // Ім'я слайсу
-  name: "contacts",
+  name: 'contacts',
   // Початковий стан редюсера слайсу
   initialState: {
     items: [],
     loading: false,
     error: null,
   },
+  // Додаємо обробку зовнішніх екшенів
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
   // Об'єкт case-редюсерів
   reducers: {
     // Виконається в момент старту HTTP-запиту
-    fetchInProgress(state) {
-      state.isLoading = true;
-    },
-    // Виконається якщо HTTP-запит завершився успішно
-    fetchSuccess(state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    // Виконається якщо HTTP-запит завершився з помилкою
-    fetchError(state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+    // fetchInProgress(state) {
+    //   state.isLoading = true;
+    // },
+    // // Виконається якщо HTTP-запит завершився успішно
+    // fetchSuccess(state, action) {
+    //   state.isLoading = false;
+    //   state.error = null;
+    //   state.items = action.payload;
+    // },
+    // // Виконається якщо HTTP-запит завершився з помилкою
+    // fetchError(state, action) {
+    //   state.isLoading = false;
+    //   state.error = action.payload;
+    // },
     addContact: (state, action) => {
       state.items.push(action.payload);
     },
     deleteContact: (state, action) => {
-      state.items = state.items.filter((item) => item.id !== action.payload);
+      state.items = state.items.filter(item => item.id !== action.payload);
     },
   },
 });
 
 // Експортуємо фабрики екшенів
 export const { addContact, deleteContact } = slice.actions;
-export const { fetchInProgress, fetchSuccess, fetchError } = slice.actions;
+// export const { fetchInProgress, fetchSuccess, fetchError } = slice.actions;
 
 // Експортуємо редюсер слайсу
 export default slice.reducer;
